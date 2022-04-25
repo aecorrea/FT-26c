c0041f7bd54cf9e496250cd5e9229f48d0fb9572
Agustin Correa <aecorrea92@gmail.com>
2022-04-25 15:52:02 -0300

    hw1

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,11 +2,29 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-  // algo
+
+let sum = 0;
+
+for (let i = 0; i < num.length; i++) {
+  sum += +num[i] * 2 ** (num.length -1 - i);
+}
+
+return sum; 
+
+  
+
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let bin = "";
+
+  while(num > 0) {
+    bin = num%2 + bin; 
+    num = Math.floor(num/2);
+  }
+  return bin;
+
 }
 
 // No se pueden usar: 
